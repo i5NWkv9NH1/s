22656cd6a0f834721110ef8cb26e138419f484c6
sorachandesu <sorachandesu@protonmail.com>
2022-02-27 00:20:39 +0900

    feat: update bilibili plugin

--- scripts/bilibili/feed.js
+++ scripts/bilibili/feed.js
@@ -5,8 +5,12 @@
  * @date 2021-09-22
  */
 // @ts-ignore
-var body = $response.body;
-body = JSON.parse(body);
+const url = $request.url;
+const method = $request.method;
+const getMethod = "GET";
+const notifiTitle = "bilibili-feed";
+let body = JSON.parse($response.body);
+
 var KEYWORD = [
     'banner_v5',
     'cm_v2',
@@ -21,32 +25,39 @@ var KEYWORD = [
     'ad_web_gif',
     'article',
     'vertical_av',
-		'static_banner',
-		'is_ad_loc',
-		'small_cover_v2'
+    'static_banner',
+    'is_ad_loc',
+    'small_cover_v2'
 ];
-var items = body.data.items.filter(function (item) {
-    return (KEYWORD.indexOf(item.card_type) === -1 &&
-        KEYWORD.indexOf(item.card_goto) === -1 &&
-        KEYWORD.indexOf(item.goto) === -1);
-});
-body.data.items = items;
-body.data.config = {
-    auto_refresh_time: null,
-    autoplay_card: 0,
-    column: 2,
-    feed_clean_abtest: 0,
-    home_transfer_test: 0,
-    show_inline_danmaku: 0,
-    toast: {
-        has_toast: false,
-        toast_message: '测试'
+body.data.items = body.data.items.filter(i => {
+    if (i.hasOwnProperty('card_type') && i.hasOwnProperty('card_goto')) {
+        const cardType = i.card_type;
+        const cardGoto = i.card_goto;
+        if (cardType === 'banner_v8' && cardGoto === 'banner') {
+            console.log(`轮播图去除`);
+            return false;
+        } else if (cardType === 'cm_v2' && ['ad_web_s', 'ad_av', 'ad_web_gif', 'ad_player', 'ad_phone_s'].includes(cardGoto)) {
+            // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告
+            console.log(`${cardGoto}广告去除)`);
+            return false;
+        } else if (cardType === 'small_cover_v10' && cardGoto === 'game') {
+            console.log('游戏广告去除');
+            return false;
+        } else if (cardType === 'cm_double_v9' && cardGoto === 'ad_inline_av') {
+            console.log('创作推广-大视频广告');
+            return false;
+        } else if (cardType === 'live' || cardGoto === 'live') {
+            console.log('移除直播');
+            return false;
+        }
+    } else {
+        console.log("body:" + $response.body);
+        $notification.post(notifiTitle, '推荐页', "无card_type/card_goto");
     }
-};
-body.data.items.forEach(function (item) {
-    item.track_id = '';
+    return true;
 });
+
 body = JSON.stringify(body);
 $done({
-    body: body
+    body
 });
