b641918418dc2a168da34280adc923c46526e7ef
sorachandesu <sorachande@protonmail.com>
2022-06-11 12:27:56 +0900

    feat: update

---
+++ scripts/wps.js
@@ -0,0 +1,55 @@
+/**
+ * @description wps-vip
+ * @url ^https?:\/\/sg\-account\.wps\.com\/api\/v\d{1}\/mine\/vips
+ */
+
+
+let body = JSON.parse($response.body);
+
+var obj = {
+  exp: 0,
+  level: 3,
+  privilege: [
+    { spid: "data_recover", times: 0, expire_time: 1846256142 },
+    { spid: "ocr", times: 0, expire_time: 1846256142 },
+    { spid: "pdf2doc", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_merge", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_sign", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_split", times: 0, expire_time: 1846256142 }
+  ],
+  result: "ok",
+  total_buy: 0,
+  total_cost: -30,
+  userid: body.userid,
+  vip: {
+    name: "超级会员",
+    has_ad: 0,
+    memberid: 40,
+    expire_time: 1846256142,
+    enabled: [
+      { memberid: 40, name: "超级会员", expire_time: 1846256142 },
+      { memberid: 20, name: "WPS会员", expire_time: 1846256142 },
+      { memberid: 12, name: "稻壳会员", expire_time: 1846256142 }
+    ]
+  },
+	vips: [
+		{
+			name: "超级会员",
+			has_ad: 0,
+			memberid: 40,
+			expire_time: 1846256142,
+			enabled: [
+				{ memberid: 40, name: "超级会员", expire_time: 1846256142 },
+				{ memberid: 20, name: "WPS会员", expire_time: 1846256142 },
+				{ memberid: 12, name: "稻壳会员", expire_time: 1846256142 }
+			]
+		},
+	],
+  wealth: 0,
+  expire_time: 1846256142
+};
+
+body = JSON.stringify(body);
+$done({
+	body
+});
