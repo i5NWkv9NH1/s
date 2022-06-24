21b043ac5c5cb1e141854faa8458350053ac9a20
sorachandesu <sorachandesu@protonmail.com>
2022-06-24 11:31:36 +0900

    feat: update

--- scripts/xiaocuntu.js
+++ scripts/xiaocuntu.js
@@ -6,7 +6,6 @@
 
 
  let body = JSON.parse($response.body);
- body = JSON.stringify(body);
  body.data.entities.show = {
   buyVip: true,
   fuli: true
@@ -15,7 +14,7 @@
   vipType: '会员',
   description: '剩余下载次数：999999'
  }
- 
+ body = JSON.stringify(body);
  $done({
    body
  })
