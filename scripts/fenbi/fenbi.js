9ece31c65c5afd7f877cb7a609b1adae6b29af48
sorachandesu <sloananna326@gmail.com>
2023-06-11 03:08:37 +0900

    feat: update

--- scripts/fenbi/fenbi.js
+++ scripts/fenbi/fenbi.js
@@ -17,6 +17,7 @@ let body = JSON.parse($response.body)
  * @match ^https?:\/\/market\-api\.fenbi\.com\/iphone\/v\d{1}\/assistant\/my\?
  * @match ^https?:\/\/hera\-webapp\.fenbi\.com\/iphone\/recommend\/tablist?
  * @match ^https?:\/\/tiku\.fenbi\.com\/iphone\/xingce\/banners\/v\d{1}?
+ * @match ^https?:\/\/tiku.fenbi.com\/[\W\w]+\/[\W\w]+\/course\/module\/config
  */
 switch (url) {
   case url.match(/small\_banner\?/)?.input: {
@@ -63,6 +64,48 @@ switch (url) {
 
     body.miniBanners.total = 0
     body.miniBanners.datas = []
+    break
+  }
+
+  case url.match(/course\/module\/config/)?.input: {
+    const blocks = [
+      '剩余批改',
+      '我的体系课',
+      '我的1对1',
+      '会员中心',
+      '套卷批改',
+      '新手特训',
+      '精品人工批改',
+      '免费资料',
+      '单题批改',
+      '小模考',
+      '精品课',
+      '冲刺备考群',
+      '精品服务',
+      '每日演练',
+      '线下自习室',
+      'PK',
+      '1对1',
+      '精品班',
+      '精品点评',
+      '精品面试辅导',
+      '精品模考',
+      '精品答题指导',
+      '会员周报'
+    ]
+    body.users = body.users.filter((item) => {
+      if (blocks.indexOf(item.name) === -1) {
+        return false
+      }
+      return item
+    })
+    body.cover = body.cover.filter((item) => {
+      if (blocks.indexOf(item.name) === -1) {
+        return false
+      }
+      return item
+    })
+
     break
   }
 }
