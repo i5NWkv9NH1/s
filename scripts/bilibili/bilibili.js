5996d87efad610341e92b4b72f7dc4fe1e957cfd
sorachandesu <sloananna326@gmail.com>
2023-06-25 18:15:35 +0900

    feat: update

--- scripts/bilibili/bilibili.js
+++ scripts/bilibili/bilibili.js
@@ -10,24 +10,67 @@ let body = JSON.parse($response.body)
 
 /**
  * @match ^https?:\/\/ap(p|i)\.bilibili\.com\/x\/v\d{1}\/feed\/index\?^https?:\/\/ap(p|i)\.bilibili\.com\/x\/v\d{1}\/feed\/index\?
+ * @match ^https?:\/\/ap(p|i)\.bilibili\.com\/x\/v\d{1}\/search\?
  */
 console.log('bilibili:: starting match')
 switch (url) {
   case url.match(/feed/)?.input: {
-    console.log('bilibili:: match feed')
-    // prettier-ignore
-    var keywords = [
-      'banner_v5','banner_v8', 'banner', 'cm_v2','large_cover_v6','small_cover_v10', 'cm_double_v9', 'ad_inline_av',  'game', 'small_cover_v9','large_cover_v1','banner','ad_web_s','ad_phone_s','live','bangumi','ad_web_gif', 'ad_ad', 'ad_player', 'ad_phone_s', 'article','vertical_av','static_banner','is_ad_loc'
-    ];
-    body.data.items = body.data.items.filter((item) => {
+    const params = new URL(url)
+    const device = params.searchParams.get('device')
+    const deviceName = params.searchParams('device_name')
+    const mobiApp = params.searchParams('mobi_app')
+
+    const isPad =
+      device.includes('pad') ||
+      deviceName.includes('pad') ||
+      mobiApp.includes('ipad')
+
+    if (isPad) {
       // prettier-ignore
-      const isCardTypeExist = keywords.indexOf(item.card_type) !== -1
-      const isCardGoExist = keywords.indexOf(item.card_goto) !== -1
-      if (isCardTypeExist || isCardGoExist) {
-        return false
-      } else {
-        return true
-      }
+      const keywords = [
+        'banner_v5','banner_v8', 'banner', 'cm_v2', 'cm_double_v9', 'ad_inline_av',  'game' ,'banner','ad_web_s','ad_phone_s','live','bangumi','ad_web_gif', 'ad_ad', 'ad_player', 'ad_phone_s', 'article','vertical_av','static_banner','is_ad_loc', 'banner_ipad_v8', 'cm_v1', 'ad_av'
+      ];
+
+      body.data.items = body.data.items.filter((item) => {
+        // prettier-ignore
+        const isCardTypeExist = keywords.indexOf(item.card_type) !== -1
+        const isCardGoExist = keywords.indexOf(item.card_goto) !== -1
+        if (isCardTypeExist || isCardGoExist) {
+          return false
+        } else {
+          return true
+        }
+      })
+      body.data.config.ipad_hd_abtest = 0
+      body.data.config.enable_rcmd_guide = false
+      body.data.config.show_inline_danmaku = 0
+
+      break
+    } else {
+      console.log('bilibili:: match feed')
+      // prettier-ignore
+      const keywords = [
+        'banner_v5','banner_v8', 'banner', 'cm_v2', 'cm_double_v9', 'ad_inline_av',  'game' ,'banner','ad_web_s','ad_phone_s','live','bangumi','ad_web_gif', 'ad_ad', 'ad_player', 'ad_phone_s', 'article','vertical_av','static_banner','is_ad_loc', 'banner_ipad_v8', 'cm_v1', 'ad_av'
+      ];
+      body.data.items = body.data.items.filter((item) => {
+        // prettier-ignore
+        const isCardTypeExist = keywords.indexOf(item.card_type) !== -1
+        const isCardGoExist = keywords.indexOf(item.card_goto) !== -1
+        if (isCardTypeExist || isCardGoExist) {
+          return false
+        } else {
+          return true
+        }
+      })
+      break
+    }
+  }
+  case url.match(/search/)?.input: {
+    var keywords = ['ad', 'game_ad', 'video_ad']
+    body.data.item = body.data.item.filter((item) => {
+      const isAd = keywords.indexOf(item.linktype) !== -1
+      if (isAd) return false
+      else return true
     })
     break
   }
--- scripts/gongkaoleida/gongkaoleida.js
+++ scripts/gongkaoleida/gongkaoleida.js
@@ -3,22 +3,30 @@
  * @desc 公考雷达
  * @date 2023-05-01
  */
-const url = $request.url;
-if (!$response.body) $done({});
+const url = $request.url
+if (!$response.body) $done({})
 
-let body = JSON.parse($response.body);
+let body = JSON.parse($response.body)
 
 /**
  * @match ^https?:\/\/api\.gongkaoleida\.com\/api\/v\d{1}\_\d{1}\_\d{1}\/index?
  * @match ^https?:\/\/api\.gongkaoleida\.com\/api\/v\d{1}\_\d{1}\/my\/index\?
  * @match ^https?:\/\/api\.gongkaoleida\.com\/api\/v\d{1}\_\d{1}\_\d{1}\/user\/vip\/product
  * @match ^https?:\/\/api\.gongkaoleida\.com\/api\/v\d{1}\_\d{1}\_\d{1}\/user\/getInfo?
+ * @match ^https?:\/\/api\.gongkaoleida\.com\/api\/v\d{1}\_\d{1}\_\d{1}\/my\/vipCardInfo?
  */
 switch (url) {
-  case url.match(/index/)?.input: {
-    const keywords = ['签到', '助考锦鲤', '选调生'];
+  case url.match(/v\d{1}\_\d{1}\_\d{1}\/index?/)?.input: {
+    const keywords = [
+      '签到',
+      '助考锦鲤',
+      '选调生',
+      '领券专区',
+      '专业库',
+      '免费资料'
+    ]
 
-    body.data.bottomSlogan = '';
+    body.data.bottomSlogan = ''
     body.data.bottomShare = {
       title: '',
       shareInfo: {
@@ -29,45 +37,78 @@ switch (url) {
       },
       tag: '',
       scheme: ''
-    };
+    }
     body.data.blockList[0].list = body.data.blockList[0].list.filter(
       (item) => keywords.indexOf(item.name) === -1
-    );
-    break;
+    )
+    break
   }
   case url.match(/my\/index/)?.input: {
-    body.data.btns = [];
-    break;
+    body.data.btns = []
+    break
   }
   case url.match(/user\/vip\/product/)?.input: {
-    const remainDays = 360;
-    const vipExpire = 1704788322;
+    const remainDays = 360
+    const vipExpire = 1704788322
 
     body.data.productList.forEach((product) => {
       product.list.forEach((item) => {
-        item.labelIsShow = 0;
-      });
-    });
+        item.labelIsShow = 0
+      })
+    })
 
-    const index = body.data.productList.findIndex(
-      (item) => item.vipGrade === 2
-    );
+    const index = body.data.productList.findIndex((item) => item.vipGrade === 2)
 
     if (index !== -1) {
-      body.data.productList[1].isVip = 1;
-      body.data.productList[1].remainDays = remainDays;
-      body.data.productList[1].vipExpire = vipExpire;
+      body.data.productList[1].isVip = 1
+      body.data.productList[1].remainDays = remainDays
+      body.data.productList[1].vipExpire = vipExpire
     }
-    break;
+    break
   }
   case url.match(/user\/getInfo/)?.input: {
-    body.data.userInfo.vipGrade = 2;
-    body.data.userInfo.isVip = 1;
-    body.data.userInfo.vipExire = 2650197302;
-    body.data.userInfo.vipGradeList[1].vipExpire = 2650197302;
-    body.data.userInfo.vipGradeList[1].isVip = 1;
-    body.data.userInfo.vipGradeList[1].remainDays = 999999;
+    body.data.userInfo.vipGrade = 2
+    body.data.userInfo.isVip = 1
+    body.data.userInfo.vipExire = 1688313599
+    body.data.userInfo.vipGradeList = [
+      {
+        vipExpire: 0,
+        vipGradeName: '黄金VIP',
+        vipGrade: 1,
+        remainDays: 0,
+        isVip: 0
+      },
+      {
+        vipExpire: 1688313599,
+        vipGradeName: '星钻VIP',
+        vipGrade: 2,
+        remainDays: 8,
+        isVip: 1
+      }
+    ]
+    break
+  }
+  case url.match(/my\/vipCardInfo/)?.input: {
+    body.data = {
+      btnScheme: 'gkld://gongkaoleida/vipMemberCenter?param1=1&param2=167',
+      greetingMsg: '尊贵的星钻VIP用户，下午好~',
+      bottomType: 1,
+      btnText: '立即查看',
+      vipGrade: 2,
+      bottomList: [
+        {
+          textColor: '#5C342A',
+          id: 6,
+          schemeUrl:
+            'gkld://gongkaoleida/vipMemberCenter?type=2&param3=13&param1=1&param2=169',
+          iconUrl:
+            'https://static.gongkaoleida.com/2022/media/images/20221209172622HAEo3V.png',
+          text: '一键筛选编制岗，星钻VIP端午福利价>>'
+        }
+      ]
+    }
+    break
   }
 }
 
-$done({ body: JSON.stringify(body) });
+$done({ body: JSON.stringify(body) })
